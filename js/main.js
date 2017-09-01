b8d7f095529b9b8198a1df024c7f483e7a1fc4bc
Catofsky <thecatofsky@gmail.com>
2017-09-01 21:59:48 +0600

    scrollbar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(e) {
+  
+  window.sc = document.querySelector('.scrollbar');
+  var chat = document.querySelector('#chat');
+  window.mes = document.querySelector('.messages');
 
-  var block = document.querySelector('.messages');
+  // var offset = mes.scrollTop;
+  // var total = mes.scrollHeight;
+  // var height = mes.clientHeight;
 
-  speed = 0;
+  var resize = function () {
+    sc.style.left = (chat.offsetLeft + 703) + 'px';
+    sc.style.height = (mes.clientHeight * mes.clientHeight / mes.scrollHeight) + 'px';
+    sc.style.top = (89 + (mes.scrollTop * mes.clientHeight / mes.scrollHeight)) + 'px';
+  }
 
-  setInterval(function () {
-    block.scrollTop += speed;
+  resize();
+  window.onresize = resize;
 
-    if (Math.floor(speed) > 0)
-      speed--;
-    else if (Math.floor(speed) < 0)
-      speed++;
+  mes.onscroll = function () {
+    sc.style.top = (89 + (mes.scrollTop * mes.clientHeight / mes.scrollHeight)) + 'px';
+  }
 
-  }, 16);
+  sc.ondrag = function (e) {
+    console.log(e);
+    sc.style.top = sc.style.offsetTop + e.movementY + 'px';
+  }
 
-  block.onwheel = function (e) {
-    if ((speed > 0 && e.deltaY < 0) || speed < 0 && e.deltaY > 0)
-      speed = 0;
+  sc.onmousedown = function f(e) {
+    console.log(e);
 
-    speed += e.deltaY / 16;
+    f.layerY = e.layerY;
+
+    move(e);
+
+    document.onmousemove = function(e) {
+      if (e.buttons != 0)
+        move(e);
+      else
+        end();
+    }
+
+    function move(e) {
+      var y = e.pageY - f.layerY;
+      if (y >= 89)
+        sc.style.top = y + 'px';
+      else
+        sc.style.top = '89px';
+    }
+
+    function end() {
+      document.onmousemove = null;
+      sc.onmouseup = null;
+    }
+
+    sc.onmouseup = function() {
+      end();
+    }
   }
 
 });
+
